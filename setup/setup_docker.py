fa7e551eaccb995c7b0e05255383f286be89f0ae
ImreSamu <ImreSamu@users.noreply.github.com>
2018-03-15 16:27:46 +0100

    add service_restart scripts and improve service_create

--- setup/setup_docker.py
+++ setup/setup_docker.py
@@ -83,11 +83,13 @@ def run_config_gen():
             gentemplate(items,            'template/service_create.sh.jinja2',     '/osm/service/'+ CONTINENT +'/' ,   'config_all_service_create.sh.txt'   )
             gentemplate(items,            'template/service_up.sh.jinja2',         '/osm/service/'+ CONTINENT +'/' ,   'config_all_service_up.sh.txt'   )
             gentemplate(items,            'template/service_down.sh.jinja2',       '/osm/service/'+ CONTINENT +'/' ,   'config_all_service_down.sh.txt'   )
+            gentemplate(items,            'template/service_restart.sh.jinja2',    '/osm/service/'+ CONTINENT +'/' ,   'config_all_service_restart.sh.txt'   )
 
             gentemplate(items_tagservice, 'template/job.sh.jinja2',                '/osm/service/'+ CONTINENT +'/' ,   'service_job.sh'   )
             gentemplate(items_tagservice, 'template/service_create.sh.jinja2',     '/osm/service/'+ CONTINENT +'/' ,   'service_create.sh'   )
             gentemplate(items_tagservice, 'template/service_up.sh.jinja2',         '/osm/service/'+ CONTINENT +'/' ,   'service_up.sh'   )
             gentemplate(items_tagservice, 'template/service_down.sh.jinja2',       '/osm/service/'+ CONTINENT +'/' ,   'service_down.sh'   )
+            gentemplate(items_tagservice, 'template/service_restart.sh.jinja2',    '/osm/service/'+ CONTINENT +'/' ,   'service_restart.sh'   )
 
             gentemplate(items_tagservice, 'template/index.md.jinja2',              '/osm/service/'+ CONTINENT +'/' ,   'service_index.md'   )
 
